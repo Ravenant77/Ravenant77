475d3feaf63536048ea837e982c687f3a4b32656
Ravenant77 <mariellano23@gmail.com>
2025-04-30 03:08:40 +0800

    Update ai.js

--- plugins/commands/ai/ai.js
+++ plugins/commands/ai/ai.js
@@ -2,19 +2,18 @@ import a from 'axios';
 const config = { name: "ai", aliases: ["ask"], permissions: [0], cooldown: 5, description: "Ask questions.", credits: "jm" };
 
 function onCall({ message: m, args: ar }) {
- const system= "";
   const q = ar.join(" ");
   if (!q) return m.reply("ask a question.");
   
   m.react("⏳");
-  a.get(`https://rapido.zetsu.xyz/api/ai?q=${encodeURIComponent(q)}?uid=${m.senderID}&system=${system}`)
+  a.get(`https://rapido.zetsu.xyz/api/ai?q=${encodeURIComponent(q)}&uid=${m.senderID}`)
     .then(res => {
       m.react("✅");
       m.reply(res.data.response);
     })
     .catch(() => {
       m.react("❌");
-      m.reply("error.");
+      m.reply("error.);
     });
 }
 
